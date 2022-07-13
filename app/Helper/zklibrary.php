b2bffbdfbd5a2e927571649705acc60c4d99b8ff
Septian Wijaya Aminulloh <septianwijaya170845@gmail.com>
2022-07-13 09:51:56 +0700

    change zklib

--- app/Helper/zklibrary.php
+++ app/Helper/zklibrary.php
@@ -139,12 +139,12 @@ class ZKLibrary
 
     public function setTimeout($sec = 0, $usec = 0)
     {
-        if ($sec != 0) {
-            $this->timeout_sec = $sec;
-        }
-        if ($usec != 0) {
-            $this->timeout_usec = $usec;
-        }
+        // if ($sec != 0) {
+        //     $this->timeout_sec = $sec;
+        // }
+        // if ($usec != 0) {
+        //     $this->timeout_usec = $usec;
+        // }
         
         $timeout = array('sec' => 5, 'usec' => 500000);
         socket_set_option($this->socket, SOL_SOCKET, SO_SNDTIMEO, $timeout);
